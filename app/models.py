658f7b0dd74921721e2421021d35300c80bf4b5d
GUSTAVO DOS SANTOS MENEZES SILVA <menezes.gustavo@aluno.senac.rio>
2026-09-23 21:44:59 -0300

    aula 10

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# tipo string = ChartField(max_length=*)
+# tipo inteiro = IntegerField()
+# tipo decimal = FloatField()
+# tipo booleano = BooleanField()
+
+class Produtos(models.Model): # o certo é PRODUTO, no sigular
+    nome = models.CharField(max_length=200)
+    preco = models.FloatField()
+    estoque = models.IntegerField()
+    imagem = models.CharField(max_length=500, default='')
+
+    def __str__(self):
+        return self.nome
