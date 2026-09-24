10cd64db0a77bdb1fd9029fabb0953aff506bf64
PununuDev <gustmenezes66.gmail.com>
2026-09-24 17:33:56 -0300

    aula 10 finalizada

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# tipo string = ChartField(max_length=*)
-# tipo inteiro = IntegerField()
-# tipo decimal = FloatField()
-# tipo booleano = BooleanField()
+# tipo string -> CharField(max_length=*)
+# tipo inteiro -> IntegerField()
+# tipo decimal -> FloatField()
+# tipo booleano -> BooleanField()
 
-class Produtos(models.Model): # o certo é PRODUTO, no sigular
+class Produtos(models.Model): # o certo é PRODUTO , no singular
     nome = models.CharField(max_length=200)
     preco = models.FloatField()
     estoque = models.IntegerField()
